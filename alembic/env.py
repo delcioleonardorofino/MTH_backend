423e67309c3fc3ab40c1863333f60e01d214e80e
unknown <delcioleonard>
2026-05-26 05:19:58 -0700

    Changed .env.py for alembic to use a sync postgres engine to run migrations

--- alembic/env.py
+++ alembic/env.py
@@ -1,50 +1,35 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import create_engine
+from sqlalchemy import pool
 
 from alembic import context
 
 from app.core.config import DATABASE_URL
 from app.models.base import Base
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
 config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 target_metadata = Base.metadata
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+def get_sync_url():
+    url = config.get_main_option("sqlalchemy.url")
+
+    if url.startswith("postgresql+asyncpg"):
+        url = url.replace("postgresql+asyncpg", "postgresql", 1)
+
+    return url
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
+    url = get_sync_url()
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,21 +42,17 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+    url = get_sync_url()
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
+    connectable = create_engine(
+        url,
+        poolclass=pool.NullPool
     )
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata
         )
 
         with context.begin_transaction():
---
+++ vars.md
@@ -0,0 +1,2 @@
+postgresql+asyncpg://neondb_owner:npg_MYV9n6iQhtAB@ep-fragrant-field-aqrnckon-pooler.c-8.us-east-1.aws.neon.tech/neondb?ssl=require
+
