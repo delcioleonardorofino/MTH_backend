7e6639513d265231990b1b06d04157db7de45ed4
unknown <delcioleonard>
2026-05-26 06:09:36 -0700

    refactor(auth): unify async database usage in OAuth flow and FastAPI router

--- app/modules/auth/router.py
+++ app/modules/auth/router.py
@@ -1,18 +1,24 @@
 from fastapi import APIRouter, Request, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from .utils import handle_redirect, handle_user_login
 from app.core.database import get_db
-from sqlalchemy.orm import Session
 
 router = APIRouter()
 
-# Endpoint por onde o user comeca o fluxo de login
-@router.get('/login/github')
+
+@router.get("/login/github")
 async def login_github(request: Request):
     return await handle_redirect(request=request)
-    
 
-# Depois de acessar /login/github o user sera redirected para a seguinte url tal como definido em redirect_uri = request.url_for('github_callback'). o github_callback deve ser o nome do endpoint abaixo.
 
-@router.get('/auth/github/callback', name='github_callback')
-async def github_callback(request: Request, db : Session = Depends(get_db)):
-    return await handle_user_login(request=request, db=db)
+@router.get("/auth/github/callback", name="github_callback")
+async def github_callback(
+    request: Request,
+    db: AsyncSession = Depends(get_db)
+):
+    return await handle_user_login(
+        provider="github",
+        request=request,
+        db=db
+    )
